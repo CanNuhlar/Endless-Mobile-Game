c391e5233e51871f63ed2360109d17c11012f42a
Can Nuhlar <cannuhlar@gmail.com>
2016-05-27 22:30:30 +0300

    Fixed a bug in collision detection

--- main.lua
+++ main.lua
@@ -15,15 +15,26 @@
     local obstacle05Left -- which is out of canvas at the init
     local randomY01
     local randomdistancebetweenObject1
+    local randomdistancebetweenObject2
+    local randomdistancebetweenObject3
+    local randomdistancebetweenObject4
+    local randomdistancebetweenObject5
+    local randomY02
+    local randomX03
+    local randomY03
+    local randomX04
+    local randomY04
+    local randomX05
+    local randomY05
 
     local physics = require ("physics")
     physics.start( )
-    --physics.setDrawMode( "hybrid" )
+    physics.setDrawMode( "hybrid" )
 
     --function to draw idiotic player
     local function addPlayer()
     	local playerSprite = {type = "image",filename="player.png"}
-    	playerRect = display.newRect(540, 1700, 75, 75)
+    	playerRect = display.newCircle(540, 1700, 35)
     	playerRect.fill = playerSprite
     end
 
@@ -67,8 +78,8 @@
     	obstacle01Left:setFillColor(obstacle01Left.r,obstacle01Left.g,obstacle01Left.b)
     	obstacle01Right:setFillColor(obstacle01Left.r,obstacle01Left.g,obstacle01Left.b)
 
-    	obstacle02Left:setFillColor(obstacle02Left.r,obstacle02Left.g,obstacle02Left.b)
-    	obstacle02Right:setFillColor(obstacle02Left.r,obstacle02Left.g,obstacle02Left.b)
+    	--obstacle02Left:setFillColor(obstacle02Left.r,obstacle02Left.g,obstacle02Left.b)
+    	--obstacle02Right:setFillColor(obstacle02Left.r,obstacle02Left.g,obstacle02Left.b)
 
     	obstacle03Left:setFillColor(obstacle03Left.r,obstacle03Left.g,obstacle03Left.b)
     	obstacle03Right:setFillColor(obstacle03Left.r,obstacle03Left.g,obstacle03Left.b)
@@ -127,36 +138,93 @@
         	obstacle01Left.y = obstacle01Left.y + scrollSpeed + 3.8
         	obstacle01Right.y = obstacle01Right.y + scrollSpeed + 3.8
         	if obstacle01Left.y > 1920 then
-        		obstacle01Left.y = 0
-        		obstacle01Right.y = 0
+        		obstacle01Left.y = -500
+        		obstacle01Right.y = -500
+        		physics.removeBody( obstacle01Left )
+    		 	physics.removeBody( obstacle01Right )
+        		physics.addBody(obstacle01Left, "static")
+    			physics.addBody(obstacle01Right, "static")
         		--print("skor+")
         	end
         	obstacle02Left.y = obstacle02Left.y + scrollSpeed + 3.8
         	obstacle02Right.y = obstacle02Right.y + scrollSpeed + 3.8
         	if obstacle02Left.y > 1920 then
-        		obstacle02Left.y = 0
-        		obstacle02Right.y = 0
+        		--obstacle02Left.y = 0
+        		--obstacle02Right.y = 0
+        		randomdistancebetweenObject2 = math.random(840,900)
+        		randomY02 = math.random(100,740) 
+    			randomX02 = math.random(-350,-200)
+    			obstacle02Left.y = obstacle01Left.y -500
+    			print(obstacle01Left.y)
+    			obstacle02Left.width = randomY02
+    			obstacle02Right.y = obstacle01Left.y -500
+    		 	obstacle02Right.width = randomdistancebetweenObject2 - randomY02
+    		 	physics.removeBody( obstacle02Left )
+    		 	physics.removeBody( obstacle02Right )
+    		 	physics.addBody(obstacle02Left, "static")
+   	 			physics.addBody(obstacle02Right, "static")
         	end
         	obstacle03Left.y = obstacle03Left.y + scrollSpeed + 3.8
         	obstacle03Right.y = obstacle03Right.y + scrollSpeed + 3.8
         	if obstacle03Left.y > 1920 then
-        		obstacle03Left.y = 0
-        		obstacle03Right.y = 0
+        		--obstacle03Left.y = 0
+        		--obstacle03Right.y = 0
+        		randomdistancebetweenObject3 = math.random(840,900)
+        		randomY03 = math.random(100,740)
+    			randomX03 = math.random(-900,-750)
+    			obstacle03Left.y = obstacle02Left.y -500
+    			obstacle03Left.width = randomY03
+    			obstacle03Right.y = obstacle02Left.y -500
+    			obstacle03Right.width = randomdistancebetweenObject3 - randomY03
+    			physics.removeBody( obstacle03Left )
+    		 	physics.removeBody( obstacle03Right )
+    		 	physics.addBody(obstacle03Left, "static")
+   	 			physics.addBody(obstacle03Right, "static")
         	end
         	obstacle04Left.y = obstacle04Left.y + scrollSpeed + 3.8
         	obstacle04Right.y = obstacle04Right.y + scrollSpeed + 3.8
         	if obstacle04Left.y > 1920 then
-        		obstacle04Left.y = 0
-        		obstacle04Right.y = 0
+        		--obstacle04Left.y = 0
+        		--obstacle04Right.y = 0
+        		randomdistancebetweenObject4 = math.random(840,900)
+        		randomY04 = math.random(100,740)
+    			randomX04 = math.random(-1450,-1300)
+    			obstacle04Left.y = obstacle03Left.y -500
+    			obstacle04Left.width = randomY04
+    			obstacle04Right.y = obstacle03Left.y -500
+    			obstacle04Right.width = randomdistancebetweenObject4 - randomY04
+    			physics.removeBody( obstacle04Left )
+    		 	physics.removeBody( obstacle04Right )
+    		 	physics.addBody(obstacle04Left, "static")
+   	 			physics.addBody(obstacle04Right, "static")
         	end
         	obstacle05Left.y = obstacle05Left.y + scrollSpeed + 3.8
         	obstacle05Right.y = obstacle05Right.y + scrollSpeed + 3.8
-        	if obstacle01Left.y > 1920 then
-        		obstacle05Left.y = 0
-        		obstacle05Right.y = 0
+        	if obstacle05Left.y > 1920 then
+        		--obstacle05Left.y = 0
+        		--obstacle05Right.y = 0
+        		randomdistancebetweenObject5 = math.random(840,900)
+        		randomY05 = math.random(100,740)
+    			randomX05 = math.random(-2000,-1850)
+    			obstacle05Left.y = obstacle04Left.y - 500
+    			obstacle05Left.width = randomY05
+    			obstacle05Right.y = obstacle04Left.y -500
+    			obstacle05Right.width = randomdistancebetweenObject5 - randomY05
+    			physics.removeBody( obstacle05Left )
+    		 	physics.removeBody( obstacle05Right )
+    		 	physics.addBody(obstacle05Left, "static")
+   	 			physics.addBody(obstacle05Right, "static")
         	end
         	--print(obstacle01Right.y)
         end
+
+    	
+    	physics.addBody(obstacle03Left, "static")
+    	physics.addBody(obstacle03Right, "static")
+    	physics.addBody(obstacle04Left, "static")
+    	physics.addBody(obstacle04Right, "static")
+    	physics.addBody(obstacle05Left, "static")
+    	physics.addBody(obstacle05Right, "static")
     end
 
     --listener for collission detection
@@ -164,7 +232,8 @@
     	print("sss")
     	if(event.phase == "began") then
     		if(self.ID == "Player" and event.other.ID == "obstacle") then
-    			print("crash!")
+    			--native.showAlert( "Game Over!", "Crashed" )
+    			print("Crashed!")
     		end
     	end
     end
@@ -194,23 +263,23 @@
     
     randomY01 = math.random(100,740)
     
-    local randomY02 = math.random(100,740) -- distance between first obs. from bottom and second
-    local randomX02 = math.random(850,1000) -- distance between second level obstacles
+    randomY02 = math.random(100,740) -- distance between first obs. from bottom and second
+    randomX02 = math.random(850,1000) -- distance between second level obstacles
 
-    local randomY03 = math.random(100,740) -- distance between second obs. from bottom and third
-    local randomX03 = math.random(450,600) -- distance between third level obstacles
+    randomY03 = math.random(100,740) -- distance between second obs. from bottom and third
+    randomX03 = math.random(450,600) -- distance between third level obstacles
 
-    local randomY04 = math.random(100,740) -- distance between third obs. from bottom and fourth
-    local randomX04 = math.random(50,200) -- distance between fourth level obstacles
+    randomY04 = math.random(100,740) -- distance between third obs. from bottom and fourth
+    randomX04 = math.random(50,200) -- distance between fourth level obstacles
 
-    local randomY05 = math.random(100,740) -- distance between fifth obs. from bottom and fifth
-    local randomX05 = math.random(-350,-200) -- out of canvas when first created
+    randomY05 = math.random(100,740) -- distance between fifth obs. from bottom and fifth
+    randomX05 = math.random(-350,-200) -- out of canvas when first created
 
     randomdistancebetweenObject1 = math.random(840,900) -- distance between first obs. from bottom
-    local randomdistancebetweenObject2 = math.random(840,900) -- distance between second obs. from bottom
-    local randomdistancebetweenObject3 = math.random(840,900) -- distance between third obs. from bottom
-    local randomdistancebetweenObject4 = math.random(840,900) -- distance between fourth obs. from bottom
-	local randomdistancebetweenObject5 = math.random(840,900) -- distance between fourth obs. from bottom
+    randomdistancebetweenObject2 = math.random(840,900) -- distance between second obs. from bottom
+    randomdistancebetweenObject3 = math.random(840,900) -- distance between third obs. from bottom
+    randomdistancebetweenObject4 = math.random(840,900) -- distance between fourth obs. from bottom
+	randomdistancebetweenObject5 = math.random(840,900) -- distance between fourth obs. from bottom
     -- first set of obstacles
     obstacle01Left = display.newRect(0, 1300, randomY01, 150)
     obstacle01Left.anchorX = 0
@@ -261,7 +330,7 @@
     obstacle05Left.align = "left"
     obstacle05Left.ID = "obstacle"
 
-    obstacle05Right = display.newRect(display.contentWidth, randomX05, randomdistancebetweenObject4 - randomY05, 150)
+    obstacle05Right = display.newRect(display.contentWidth, randomX05, randomdistancebetweenObject5 - randomY05, 150)
     obstacle05Right.anchorX = 1
     obstacle05Right.align = "right"
     obstacle05Right.ID = "obstacle"
@@ -285,7 +354,7 @@
     		display.getCurrentStage():setFocus( self )
     		self.markX = self.x
     		print("began")
-    	elseif event.phase == "moved" then
+    	elseif event.phase == "moved" and self.markX ~= 		nil  then
     		local x = (event.x - event.xStart) + self.markX
     		--print(x)
     		if x < 120 then
